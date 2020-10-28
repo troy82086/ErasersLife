44a566b302fe0317531688c162881d0f256cc7a6
troy82086 <turner82086@gmail.com>
2020-10-28 03:29:48 -0600

    Main menu and more of the town

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ExitGame();
+        }
+    }
+
+    public void StartGame()
+    {
+
+    }
+
+    public void Settings()
+    {
+
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
