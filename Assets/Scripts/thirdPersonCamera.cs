9c370f561cce2558487f766a45d0c8fc17079253
troy82086 <turner82086@gmail.com>
2020-11-04 12:25:44 -0700

    Update thirdPersonCamera.cs

--- Assets/Scripts/thirdPersonCamera.cs
+++ Assets/Scripts/thirdPersonCamera.cs
@@ -4,30 +4,22 @@ using UnityEngine;
 
 public class thirdPersonCamera : MonoBehaviour
 {
-    [SerializeField] GameObject[] targets;
-    [SerializeField] float rotateSpeed = 5;
-    
-    private Vector3 offset;
-    private GameObject target;
-
-    private void Start()
-    {
-        target = targets[2];
+    public GameObject target;
+    public float rotateSpeed = 5;
+    Vector3 offset;
+     
+    void Start() {
         offset = target.transform.position - transform.position;
     }
-
-    void LateUpdate()
-    {
-        //for(int i = 0; i < targets.Length; i++) if (targets[i].activeInHierarchy) target = targets[i];
-        //if(offset != target.transform.position - transform.position) offset = target.transform.position - transform.position;
-        
+     
+    void LateUpdate() {
         float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
         target.transform.Rotate(0, horizontal, 0);
-
+ 
         float desiredAngle = target.transform.eulerAngles.y;
         Quaternion rotation = Quaternion.Euler(0, desiredAngle, 0);
         transform.position = target.transform.position - (rotation * offset);
-
+         
         transform.LookAt(target.transform);
     }
 }
