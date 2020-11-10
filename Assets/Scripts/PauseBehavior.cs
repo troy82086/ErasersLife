90377d31653ec5107b7da43719428db0de3bfa63
troy82086 <45493654+troy82086@users.noreply.github.com>
2020-11-10 15:12:06 -0700

    Start of a Pause menu

---
+++ Assets/Scripts/PauseBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseBehavior : MonoBehaviour
+{
+    [SerializeField] Canvas pauseCanvas = null;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log(pauseCanvas.enabled);
+            pauseCanvas.enabled = !pauseCanvas.enabled;
+        }
+
+        if (pauseCanvas.enabled) Time.timeScale = 0;
+        if (!pauseCanvas.enabled) Time.timeScale = 1;
+
+
+    }
+}
