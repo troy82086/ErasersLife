eabd99d71441ec9d3a36de9142cade088cdec867
troy82086 <45493654+troy82086@users.noreply.github.com>
2020-11-10 15:11:55 -0700

    Start Cutscene

---
+++ Assets/Scripts/StartingCutsceneScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class StartingCutsceneScript : MonoBehaviour
+{
+    [SerializeField] Rigidbody planeRB = null;
+    [SerializeField] PlayerWalk walking = null;
+    [SerializeField] PlayerCar car = null;
+    [SerializeField] PlayerPlane plane = null;
+
+    void Update()
+    {
+        if(planeRB != null && GetComponent<PlayableDirector>().time == 0)
+        {
+            planeRB.isKinematic = false;
+            walking.enabled = true;
+            car.enabled = true;
+            plane.enabled = true;
+        }
+        else
+        {
+            planeRB.isKinematic = true;
+            walking.enabled = false;
+            car.enabled = false;
+            plane.enabled = false;
+        }
+    }
+}
