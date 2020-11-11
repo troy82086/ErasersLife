5524e031ef6b32c2483dca16a32af159799a0916
troy82086 <45493654+troy82086@users.noreply.github.com>
2020-11-11 13:00:47 -0700

    Begining Cutscene and Settings

---
+++ Assets/Scripts/SettingsValues.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsValues : MonoBehaviour
+{
+    [SerializeField] Slider mini = null;
+    [SerializeField] Slider vol = null;
+    [SerializeField] TMP_Dropdown iM = null;
+
+    private int miniPov = 300;
+    private int volume = 50;
+    private int impericalOrMetric = 1;
+
+    private void Awake()
+    {
+        mini.value = miniPov;
+        vol.value = volume;
+        iM.value = impericalOrMetric;
+    }
+
+    void Update()
+    {
+        if (miniPov != mini.value) miniPov = (int)mini.value;
+        if (volume != vol.value) volume = (int)vol.value;
+        if (impericalOrMetric != iM.value) impericalOrMetric = iM.value;
+    }
+}
--- Assets/Scripts/StartingCutsceneScript.cs
+++ Assets/Scripts/StartingCutsceneScript.cs
@@ -9,6 +9,7 @@ public class StartingCutsceneScript : MonoBehaviour
     [SerializeField] PlayerWalk walking = null;
     [SerializeField] PlayerCar car = null;
     [SerializeField] PlayerPlane plane = null;
+    [SerializeField] Camera[] cameras = null;
 
     void Update()
     {
@@ -18,6 +19,10 @@ public class StartingCutsceneScript : MonoBehaviour
             walking.enabled = true;
             car.enabled = true;
             plane.enabled = true;
+            cameras[0].enabled = true;
+            cameras[1].enabled = true;
+            cameras[2].enabled = true;
+            cameras[3].enabled = false;
         }
         else
         {
@@ -25,6 +30,10 @@ public class StartingCutsceneScript : MonoBehaviour
             walking.enabled = false;
             car.enabled = false;
             plane.enabled = false;
+            cameras[0].enabled = false;
+            cameras[1].enabled = false;
+            cameras[2].enabled = false;
+            cameras[3].enabled = true;
         }
     }
 }
