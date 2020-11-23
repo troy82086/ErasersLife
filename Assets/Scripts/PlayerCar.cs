ea8504eb0b07a24e7566c2527f5bc33479110246
troy82086 <45493654+troy82086@users.noreply.github.com>
2020-11-23 13:09:30 -0700

    Update PlayerCar.cs

--- Assets/Scripts/PlayerCar.cs
+++ Assets/Scripts/PlayerCar.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 
 public class PlayerCar : MonoBehaviour
 {
+    private const float MAX_SPEED_ANGLE = -75;
+    private const float ZERO_SPEED_ANGLE = -30;
+
+    [SerializeField] Transform needle = null;
     [SerializeField] Transform player = null;
     [SerializeField] Transform camera = null;
     [SerializeField] Transform pauseCamera = null;
     [SerializeField] int turnSpeed = 1;
-    [SerializeField] int m_speed = 1;
+    [SerializeField] float m_maxSpeed = 1;
+    [SerializeField] float m_speed = 1;
 
     public float energyUsage = 1;
     public int cost = 5;
@@ -27,16 +32,42 @@ public class PlayerCar : MonoBehaviour
         rotation.eulerAngles += new Vector3(0, h, 0);
         player.transform.rotation = Quaternion.Lerp(player.rotation, rotation, Time.deltaTime * 10);
 
-        float forward = Input.GetAxis("Vertical") * m_speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.W) && m_speed < m_maxSpeed)
+        {
+            m_speed += 0.1f;
+        }
+        else if (Input.GetKey(KeyCode.S) && m_speed > -m_maxSpeed / 2)
+        {
+            m_speed -= 0.1f;
+        }
+        else if (m_speed > 0.01)
+        {
+            m_speed -= 0.01f;
+        }
+        else if (m_speed < -0.01)
+        {
+            m_speed += 0.01f;
+        }
+
+        float forward = m_speed;
 
         Vector3 FORWARD = player.TransformDirection(Vector3.up);
 
+        
         player.localPosition += FORWARD * forward;
-
-
+        needle.eulerAngles = new Vector3(0, 0, GetSpeedRotation());
         camera.localPosition = new Vector3(player.localPosition.x, camera.localPosition.y, player.localPosition.z);
         camera.eulerAngles = new Vector3(camera.eulerAngles.x, 0, -player.eulerAngles.y + 180);
         pauseCamera.localPosition = new Vector3(player.localPosition.x, pauseCamera.localPosition.y, player.localPosition.z);
         pauseCamera.eulerAngles = new Vector3(pauseCamera.eulerAngles.x, 0, -player.eulerAngles.y + 180);
+    }
+
+    private float GetSpeedRotation()
+    {
+        float totalAngleSize = ZERO_SPEED_ANGLE - MAX_SPEED_ANGLE;
+        float speedNormal = 0;
+        if (m_speed > 0) speedNormal = m_speed / m_maxSpeed; 
+        if (m_speed < 0) speedNormal = -m_speed / m_maxSpeed; 
+        return ZERO_SPEED_ANGLE - speedNormal * totalAngleSize;
     }
 }
