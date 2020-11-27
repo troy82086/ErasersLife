021017e5064f095c49c8d80982665769a09977c2
troy82086 <turner82086@gmail.com>
2020-11-27 10:37:57 -0700

    Volume and Sounds add

--- Assets/Scripts/SettingsValues.cs
+++ Assets/Scripts/SettingsValues.cs
@@ -3,41 +3,60 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.UI;
 
 public class SettingsValues : MonoBehaviour
 {
     [SerializeField] Slider mini = null;
-    [SerializeField] Slider vol = null;
+    [SerializeField] Slider sfxVol = null;
+    [SerializeField] Slider musicVol = null;
     [SerializeField] TMP_Dropdown iM = null;
     [SerializeField] GameObject SettingsCanvas = null;
+    [SerializeField] AudioMixer audioMixer1;
+    [SerializeField] AudioMixer audioMixer2;
 
     [Serializable]
     public static class serializeValues
     {
         public static int miniPov = 300;
-        public static int volume = 50;
+        public static float sfxVolume = 80;
+        public static float musicVolume = 80;
         public static int impericalOrMetric = 1;
     }
 
+
     private int miniPov;
-    private int volume;
+    private float sfxVolume;
+    private float musicVolume;
     private int impericalOrMetric;
 
     private void Awake()
     {
         miniPov = serializeValues.miniPov;
-        volume = serializeValues.volume;
+        sfxVolume = serializeValues.sfxVolume;
+        musicVolume = serializeValues.musicVolume;
         impericalOrMetric = serializeValues.impericalOrMetric;
         mini.value = miniPov;
-        vol.value = volume;
-        if(iM != null) iM.value = impericalOrMetric;
+        sfxVol.value = sfxVolume;
+        musicVol.value = musicVolume;
+        audioMixer1.SetFloat("sfx", sfxVolume - 40);
+        audioMixer2.SetFloat("music", musicVolume - 40);
+        if (iM != null) iM.value = impericalOrMetric;
     }
 
     public void MiniMapValueChange() { serializeValues.miniPov = (int)mini.value; }
-    public void VolumeValueChange() { serializeValues.volume = (int)vol.value; }
-    public void ImpericalOrMetricValueChange() { if(iM != null) serializeValues.impericalOrMetric = iM.value; }
+    public void SFXVolumeValueChange()
+    {
+        serializeValues.sfxVolume = sfxVol.value - 40;
+        audioMixer1.SetFloat("sfx", serializeValues.sfxVolume - 40);
+    }
+    public void MusicVolumeValueChange()
+    {
+        serializeValues.musicVolume = musicVol.value - 40;
+        audioMixer2.SetFloat("music", serializeValues.musicVolume - 40);
+    }
+    public void ImpericalOrMetricValueChange() { if (iM != null) serializeValues.impericalOrMetric = iM.value; }
 
     public int GetImpericalOrMetric() { return serializeValues.impericalOrMetric; }
-    public int GetVolume() { return serializeValues.volume; }
 }
